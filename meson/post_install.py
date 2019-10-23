7840398cf3329ac812ebdc29c3ca07557165b125
Vlad Pop <popvladaurel@gmail.com>
2019-10-23 18:42:08 +0300

    Added a trial icon

--- meson/post_install.py
+++ meson/post_install.py
@@ -6,10 +6,13 @@ import subprocess
 install_prefix = os.environ['MESON_INSTALL_PREFIX']
 schemadir = os.path.join(install_prefix, 'share/glib-2.0/schemas')
 desktop_database_dir = os.path.join(install_prefix, 'applications')
+icons_dir = os.path.join(install_prefix, 'share/icons')
 
 if not os.environ.get('DESTDIR'):
     print('Compiling the gsettings schemas...')
     subprocess.call(['glib-compile-schemas', schemadir])
     print('Updating desktop database...')
     subprocess.call(['update-desktop-database', '-q', desktop_database_dir])
+    print('Updating icon cache...')
+    subprocess.call(['update-icon-caches', icons_dir])
     print('Done!')
